d44cb1a3359c3f2d41d0eb82ef493f7ca1001fd9
Raymond Chua <ray.r.chua@gmail.com>
2026-01-15 13:20:38 -0500

    Add validation dataset loading to training script

--- train.py
+++ train.py
@@ -86,6 +86,9 @@ class Workspace:
         self.train_dset = self.dataset["train"]
         self.ds_train = self.train_dset.with_format("torch")
 
+        self.val_dset = self.dataset["test"]
+        self.ds_val = self.val_dset.with_format("torch")
+
 
         print("OK")
 
@@ -134,7 +137,7 @@ class Workspace:
         return "".join(tokens)
 
     def eval(self, epoch: int = 0):
-        dataloader_eval = DataLoader(self.ds_train, batch_size=self.cfg.batch_size, shuffle=False)
+        dataloader_eval = DataLoader(self.ds_val, batch_size=self.cfg.batch_size, shuffle=False)
         loss, acc= [], [],
         for idx, batch in enumerate(dataloader_eval):
             input_x = []
